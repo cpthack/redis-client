e524e6b3fa47e42e9e1f31b91064469e29aebe36
Administrator <Administrator@DESKTOP-NOJS0QA>
2017-04-15 20:45:56 +0800

    增加常量配置

---
+++ src/main/java/com/cpthack/commons/rdclient/constants/RedisConstants.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2013-2020, cpthack 成佩涛 (cpt@jianzhimao.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.cpthack.commons.rdclient.constants;
+
+/**
+ * <b>RedisConstants.java</b></br>
+ * 
+ * <pre>
+ * redis常量类
+ * </pre>
+ * 
+ * @author cpthack cpt@jianzhimao.com
+ * @date 2017年4月15日 下午8:37:56
+ * @since JDK 1.7
+ */
+public class RedisConstants {
+	
+}
